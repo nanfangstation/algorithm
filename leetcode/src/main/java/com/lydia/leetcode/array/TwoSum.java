837c6ce81029c363ae1b162ee560103aadf8d53d
pengnanfang <1064977639@qq.com>
2020-04-26 01:00:47 +0800

    return

--- leetcode/src/main/java/com/lydia/leetcode/array/TwoSum.java
+++ leetcode/src/main/java/com/lydia/leetcode/array/TwoSum.java
@@ -28,18 +28,19 @@ public class TwoSum {
     * */
     @Test
     public void testOne() {
-        int[] nums = {2, 7, 11, 15};
-        int target = 9;
+        int[] nums = {3, 3};
+        int target = 6;
         System.out.println(Arrays.toString(twoSum(nums, target)));
     }
 
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
+            for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
                     result[0] = i;
                     result[1] = j;
+                    return result;
                 }
             }
         }
