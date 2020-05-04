b9b8ae6ef5b7779ecbfd1750a68da9801b41cd5c
pengnanfang <1064977639@qq.com>
2020-05-04 23:58:13 +0800

    InvertBinaryTree

---
+++ leetcode/src/main/java/com/lydia/leetcode/tree/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,37 @@
+package com.lydia.leetcode.tree;
+
+/**
+ * @author Lydia
+ * @see https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * @since 2020-05-04 00:32
+ **/
+public class LowestCommonAncestorOfABinarySearchTree {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (p == null && q == null) {
+            return null;
+        }
+        // Value of current node or parent node.
+        int parentVal = root.val;
+
+        // Value of p
+        int pVal = p.val;
+
+        // Value of q;
+        int qVal = q.val;
+
+        if (pVal > parentVal && qVal > parentVal) {
+            // If both p and q are greater than parent
+            return lowestCommonAncestor(root.right, p, q);
+        } else if (pVal < parentVal && qVal < parentVal) {
+            // If both p and q are lesser than parent
+            return lowestCommonAncestor(root.left, p, q);
+        } else {
+            // We have found the split point, i.e. the LCA node.
+            return root;
+        }
+    }
+}
